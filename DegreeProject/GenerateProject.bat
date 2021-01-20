118c61b36ca422db09f0ef947223577689f2532d
Robin Ortryd <45140757+Haesst@users.noreply.github.com>
2021-01-20 09:14:14 +0100

    Added premake and fixed so gitignore doesnt block SFML dll's

---
+++ DegreeProject/GenerateProject.bat
@@ -0,0 +1,2 @@
+call ThirdParty\Premake\premake5.exe vs2019
+PAUSE
---
+++ DegreeProject/premake5.lua
@@ -0,0 +1,62 @@
+workspace "DegreeProject"
+	architecture "x86"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+-- Include directories relative to solution directory
+includedir = {}
+includedir["SFML"] = "ThirdParty/SFML-2.5.1/include"
+
+libdir = {}
+libdir["SFML"] = "ThirdParty/SFML-2.5.1/lib"
+
+sfmlbindir = "ThirdParty/SFML-2.5.1/bin"
+
+project "DegreeProject"
+	location "DegreeProject"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp",
+	}
+
+	defines
+	{
+		"SFML_DYNAMIC"
+	}
+
+	includedirs
+	{
+		"%{includedir.SFML}"
+	}
+
+	libdirs
+	{
+		"%{libdir.SFML}"
+	}
+
+	links
+	{
+		"sfml-graphics.lib",
+		"sfml-window.lib",
+		"sfml-system.lib"
+	}
+	postbuildcommands
+	{
+		("{COPY} %{wks.location}%{sfmlbindir}/sfml-graphics-2.dll %{wks.location}bin/" .. outputdir .. "/%{prj.name}"),
+		("{COPY} %{wks.location}%{sfmlbindir}/sfml-window-2.dll %{wks.location}bin/" .. outputdir .. "/%{prj.name}"),
+		("{COPY} %{wks.location}%{sfmlbindir}/sfml-system-2.dll %{wks.location}bin/" .. outputdir .. "/%{prj.name}")
+	}
