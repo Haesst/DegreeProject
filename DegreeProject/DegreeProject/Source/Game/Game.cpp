2035a8fef6a0fcb11a52ae63821ca300c94e7359
Haesst <robin.ortryd@gmail.com>
2021-05-05 09:20:03 +0200

    France DLC

--- DegreeProject/DegreeProject/Source/Game/Game.cpp
+++ DegreeProject/DegreeProject/Source/Game/Game.cpp
@@ -222,6 +222,9 @@ void Game::addEntitys()
 	addRandomEntityOwningRegion({ 52 });
 	addRandomEntityOwningRegion({ 53 });
 	addRandomEntityOwningRegion({ 54 });
+	addRandomEntityOwningRegion({ 55 });
+	addRandomEntityOwningRegion({ 56 });
+	addRandomEntityOwningRegion({ 57 });
 
 	//UI
 	Vector2D mainMenuPosition = { 960.0f, 540.0f };
