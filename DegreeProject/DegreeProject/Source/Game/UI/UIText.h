14938efa1e8ceebaab1463e87890ba3166623f66
fredrikhanses <fredrik.hanses@gmail.com>
2021-05-10 21:40:33 +0200

    Bigger realm text

--- DegreeProject/DegreeProject/Source/Game/UI/UIText.h
+++ DegreeProject/DegreeProject/Source/Game/UI/UIText.h
@@ -33,7 +33,7 @@ public:
 	std::string m_CountryName = "";
 	unsigned int m_CharacterSize = 20;
 	unsigned int m_MinCharacterSize = 30;
-	unsigned int m_MaxCharacterSize = 200;
+	unsigned int m_MaxCharacterSize = 400;
 	const float m_MinOutlineThickness = 0.1f;
 	const float m_MaxOutlineThickness = 1.0f;
 	float m_Rotation = 0.0f;
